6ffa1ebafd4aa24c0805099d85e1c04ece87b06b
bellrain21 <112729543+bellrain21@users.noreply.github.com>
2025-07-17 18:51:35 +0900

    .

--- middleProject/Back_End/src/main/java/com/todayhospital/controller/MemberController.java
+++ middleProject/Back_End/src/main/java/com/todayhospital/controller/MemberController.java
@@ -2,7 +2,6 @@ package com.todayhospital.controller;
 
 import com.todayhospital.dao.MemberDAO;
 import com.todayhospital.dto.MemberDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class MemberController {
 
